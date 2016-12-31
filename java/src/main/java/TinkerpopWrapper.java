fd90778049ab5c7841b07741eaa54133a21b2c17
kud1ing <kud1ing@users.noreply.github.com>
2016-12-31 11:13:41 +0100

    add a README, upgrade Rucaja

--- java/src/main/java/TinkerpopWrapper.java
+++ java/src/main/java/TinkerpopWrapper.java
@@ -3,15 +3,19 @@ import org.apache.tinkerpop.gremlin.tinkergraph.structure.TinkerGraph;
 
 public class TinkerpopWrapper {
 
-  public static void println(Object object) {
-    System.out.println(object);
-  }
+    public static void println(Object o) {
+        System.out.println(o);
+    }
 
-  public static Graph tinkergraph_new() {
-    return TinkerGraph.open();
-  }
+    public static void print_graph(Graph g) {
+        System.out.println(g);
+    }
 
-  public static void main(String[] args) {
-    System.out.println(tinkergraph_new());
-  }
+    public static Graph tinkergraph_new() {
+        return TinkerGraph.open();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(tinkergraph_new());
+    }
 }
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -18,12 +18,19 @@ fn main() {
     unsafe {
         let jvm = Jvm::new(&jvm_options);
 
+        // Get a reference to the Java wrapper class.
         let class = jvm.get_class("TinkerpopWrapper").expect("Could not find `TinkerpopWrapper`");
+
+        // Get references to Java methods in that Java class.
+        let tinkergraph_new = jvm.get_static_method(&class, "tinkergraph_new", "()Lorg/apache/tinkerpop/gremlin/structure/Graph;").expect("Could not find `tinkergraph_new()`");
+        let main = jvm.get_static_method(&class, "main", "([Ljava/lang/String;)V").expect("Could not find `main()`");
+        let print_graph = jvm.get_static_method(&class, "print_graph", "(Lorg/apache/tinkerpop/gremlin/structure/Graph;)V").expect("Could not find `main()`");
         let println = jvm.get_static_method(&class, "println", "(Ljava/lang/Object;)V").expect("Could not find `println()`");
 
-        let tinkergraph_new = jvm.get_static_method(&class, "tinkergraph_new", "()Lorg/apache/tinkerpop/gremlin/structure/Graph;").expect("Could not find `tinkergraph_new()`");
+        // Instantiate a Java `TinkerGraph`.
         let graph = jvm.call_static_object_method(&class, &tinkergraph_new, null());
 
+        // Print the `TinkerGraph` via Java's `System.out.println()`.
         let args = vec![jvalue_from_jobject(graph)];
         jvm.call_static_void_method(&class, &println, args.as_ptr());
     }
