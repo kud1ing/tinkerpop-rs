cd405eb1f2e5201d0dedb8a6ffed5f1a353b5074
kud1ing <kud1ing@users.noreply.github.com>
2016-12-31 12:50:37 +0100

    disable Java methods that can not be resolved

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -24,14 +24,16 @@ fn main() {
         // Get references to Java methods in that Java class.
         let tinkergraph_new = jvm.get_static_method(&class, "tinkergraph_new", "()Lorg/apache/tinkerpop/gremlin/structure/Graph;").expect("Could not find `tinkergraph_new()`");
         let main = jvm.get_static_method(&class, "main", "([Ljava/lang/String;)V").expect("Could not find `main()`");
-        let print_graph = jvm.get_static_method(&class, "print_graph", "(Lorg/apache/tinkerpop/gremlin/structure/Graph;)V").expect("Could not find `main()`");
-        let println = jvm.get_static_method(&class, "println", "(Ljava/lang/Object;)V").expect("Could not find `println()`");
+
+        // TODO: These methods can not be resolved:
+        //let print_graph = jvm.get_static_method(&class, "print_graph", "(Lorg/apache/tinkerpop/gremlin/structure/Graph;)V").expect("Could not find `print_graph()`");
+        //let println = jvm.get_static_method(&class, "println", "(Ljava/lang/Object;)V").expect("Could not find `println()`");
 
         // Instantiate a Java `TinkerGraph`.
         let graph = jvm.call_static_object_method(&class, &tinkergraph_new, null());
 
-        // Print the `TinkerGraph` via Java's `System.out.println()`.
-        let args = vec![jvalue_from_jobject(graph)];
-        jvm.call_static_void_method(&class, &println, args.as_ptr());
+        // TODO: Print the `TinkerGraph` via Java's `System.out.println()`.
+        // let args = vec![jvalue_from_jobject(graph)];
+        // jvm.call_static_void_method(&class, &println, args.as_ptr());
     }
 }
