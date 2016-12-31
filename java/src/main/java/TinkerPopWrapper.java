83ec0376ee25084e372a66a883ba753f29d3bc92
kud1ing <kud1ing@users.noreply.github.com>
2016-12-31 15:52:21 +0100

    add vertex

--- java/src/main/java/TinkerPopWrapper.java
+++ java/src/main/java/TinkerPopWrapper.java
@@ -1,10 +1,25 @@
 import org.apache.tinkerpop.gremlin.structure.Graph;
+import org.apache.tinkerpop.gremlin.structure.Vertex;
 import org.apache.tinkerpop.gremlin.tinkergraph.structure.TinkerGraph;
 
 /**
  * A wrapper class for Apache TinkerPop to be used from Rust code.
+ *
+ * The Java method signatures can be seen using the Java command-line tool `javap`:
+ *
+ *     javap -s build/classes/main/TinkerPopWrapper.class
  */
 public class TinkerPopWrapper {
+
+    /**
+     * Adds a vertex to the graph
+     *
+     * @param graph
+     * @return a new vertex
+     */
+    public static Vertex graph_add_vertex(Graph graph) {
+        return graph.addVertex();
+    }
 
     /**
      * A wrapper for `System.out.println()` that is easier to use from Rust.
@@ -16,7 +31,9 @@ public class TinkerPopWrapper {
 
     /**
      * Creates and returns a `TinkerGraph`.
-     * @return
+     *
+     * @see <http://tinkerpop.apache.org/javadocs/3.2.3/full/org/apache/tinkerpop/gremlin/tinkergraph/structure/TinkerGraph.html>
+     * @return a new `TinkerGraph` instance
      */
     public static Graph tinkergraph_new() {
         return TinkerGraph.open();
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -22,15 +22,39 @@ fn main() {
         println!("* Get a reference to the Java wrapper class `TinkerPopWrapper`.");
         let class = jvm.get_class("TinkerPopWrapper").expect("Could not find `TinkerPopWrapper`");
 
-        println!("* Get references to Java methods in that Java wrapper class.");
-        let tinkergraph_new = jvm.get_static_method(&class, "tinkergraph_new", "()Lorg/apache/tinkerpop/gremlin/structure/Graph;").expect("Could not find `tinkergraph_new()`");
-        let println = jvm.get_static_method(&class, "println", "(Ljava/lang/Object;)V").expect("Could not find `println()`");
 
-        println!("* Instantiate a Java `TinkerGraph` object");
+        println!("* Get references to Java methods in that Java wrapper class.");
+
+        let graph_add_vertex = jvm.get_static_method(
+            &class, "graph_add_vertex",
+            "(Lorg/apache/tinkerpop/gremlin/structure/Graph;)Lorg/apache/tinkerpop/gremlin/structure/Vertex;"
+        ).expect("Could not find `tinkergraph_new()`");
+
+        let tinkergraph_new = jvm.get_static_method(
+            &class, "tinkergraph_new",
+            "()Lorg/apache/tinkerpop/gremlin/structure/Graph;"
+        ).expect("Could not find `tinkergraph_new()`");
+
+        let println = jvm.get_static_method(
+            &class,
+            "println",
+            "(Ljava/lang/Object;)V"
+        ).expect("Could not find `println()`");
+
+
+        println!("* instantiate a Java `TinkerGraph` object");
         let graph = jvm.call_static_object_method(&class, &tinkergraph_new, null());
 
-        println!("* Print the `TinkerGraph` object using Java's `System.out.println()`:");
-        let args = vec![jvalue_from_jobject(graph)];
-        jvm.call_static_void_method(&class, &println, args.as_ptr());
+        {
+            println!("* add a vertex");
+            let args = vec![jvalue_from_jobject(graph)];
+            let vertex = jvm.call_static_object_method(&class, &graph_add_vertex, args.as_ptr());
+        }
+
+        {
+            println!("* print the `TinkerGraph` object using Java's `System.out.println()`:");
+            let args = vec![jvalue_from_jobject(graph)];
+            jvm.call_static_void_method(&class, &println, args.as_ptr());
+        }
     }
 }
